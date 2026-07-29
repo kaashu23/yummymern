5dca36cc8d2712a0458bb5cd55d1a4517e71ae69
kashish <kashishsalvi06@gmail.com>
2026-07-30 00:16:06 +0530

    idk

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -9,9 +9,7 @@ if (dns.setDefaultResultOrder) {
 const sendEmail = async (options) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
